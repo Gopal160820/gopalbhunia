c5c06f72689a4ab52133a205bfc9d5d0958f4680
Gopal160820 <131211923+Gopal160820@users.noreply.github.com>
2023-11-03 18:03:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,16 +6,3 @@ function hideSidebar(){
     const sidebar = document.querySelector('.sidebar')
     sidebar.style.display = 'none'
 }
-
-
-
-window.addEventListener('load', function () {
-    // Hide the loader and show the content when the page is fully loaded
-    const loader = document.querySelector('.loader-container');
-    const content = document.querySelector('.content');
-    
-    setTimeout(function () {
-        loader.style.display = 'none';
-        content.style.display = 'flex';
-    }, 2000); // Adjust the time to control how long the loader is displayed
-});
